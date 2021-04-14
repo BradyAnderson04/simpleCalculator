5a457220f8ac90c052c09116e0c5e5b1d23fc9ba
Cryptc <bradya805@gmail.com>
2021-04-14 11:40:47 -0400

    a simple calculator program complete

---
+++ example.py
@@ -0,0 +1,22 @@
+'''
+A simple calculator function
+'''
+
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+if __name__ == '__main__':
+    print(add(5, 5))
+    print(subtract(5, 5))
+    print(multiply(5, 5))
+    print(divide(5, 5))
